be7228cd90c3c032a562fce3231581a0776e7a81
Unknown <findryankurnia@gmail.com>
2018-11-12 09:19:24 +0700

    open dan membuat file baru

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,14 +29,21 @@
         private void InitializeComponent()
         {
             this.holderTop = new System.Windows.Forms.Panel();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_new = new System.Windows.Forms.Button();
             this.btn_minimize = new System.Windows.Forms.Button();
             this.btn_close = new System.Windows.Forms.Button();
             this.holderLeft = new System.Windows.Forms.Panel();
+            this.btn_rubber = new System.Windows.Forms.Button();
+            this.btn_brush = new System.Windows.Forms.Button();
+            this.btn_circle = new System.Windows.Forms.Button();
             this.holderRight = new System.Windows.Forms.Panel();
-            this.btn_new = new System.Windows.Forms.Button();
-            this.btn_open = new System.Windows.Forms.Button();
-            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_pen = new System.Windows.Forms.Button();
+            this.btn_square = new System.Windows.Forms.Button();
+            this.drawarenapanel = new System.Windows.Forms.Panel();
             this.holderTop.SuspendLayout();
+            this.holderLeft.SuspendLayout();
             this.SuspendLayout();
             // 
             // holderTop
@@ -53,6 +60,35 @@
             this.holderTop.Size = new System.Drawing.Size(799, 40);
             this.holderTop.TabIndex = 0;
             // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(126, 3);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(55, 27);
+            this.btn_save.TabIndex = 0;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(65, 3);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(55, 27);
+            this.btn_open.TabIndex = 3;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_new
+            // 
+            this.btn_new.Location = new System.Drawing.Point(4, 3);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(55, 28);
+            this.btn_new.TabIndex = 2;
+            this.btn_new.Text = "New";
+            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_new.Click += new System.EventHandler(this.btn_new_Click);
+            // 
             // btn_minimize
             // 
             this.btn_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -62,6 +98,7 @@
             this.btn_minimize.TabIndex = 1;
             this.btn_minimize.Text = "Minimize";
             this.btn_minimize.UseVisualStyleBackColor = true;
+            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
             // 
             // btn_close
             // 
@@ -78,10 +115,40 @@
             // 
             this.holderLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+            this.holderLeft.Controls.Add(this.btn_rubber);
+            this.holderLeft.Controls.Add(this.btn_brush);
+            this.holderLeft.Controls.Add(this.btn_circle);
             this.holderLeft.Location = new System.Drawing.Point(2, 39);
             this.holderLeft.Name = "holderLeft";
             this.holderLeft.Size = new System.Drawing.Size(59, 410);
             this.holderLeft.TabIndex = 1;
+            // 
+            // btn_rubber
+            // 
+            this.btn_rubber.Location = new System.Drawing.Point(4, 278);
+            this.btn_rubber.Name = "btn_rubber";
+            this.btn_rubber.Size = new System.Drawing.Size(52, 45);
+            this.btn_rubber.TabIndex = 3;
+            this.btn_rubber.Text = "Rubber";
+            this.btn_rubber.UseVisualStyleBackColor = true;
+            // 
+            // btn_brush
+            // 
+            this.btn_brush.Location = new System.Drawing.Point(4, 77);
+            this.btn_brush.Name = "btn_brush";
+            this.btn_brush.Size = new System.Drawing.Size(52, 45);
+            this.btn_brush.TabIndex = 0;
+            this.btn_brush.Text = "Brush";
+            this.btn_brush.UseVisualStyleBackColor = true;
+            // 
+            // btn_circle
+            // 
+            this.btn_circle.Location = new System.Drawing.Point(4, 227);
+            this.btn_circle.Name = "btn_circle";
+            this.btn_circle.Size = new System.Drawing.Size(52, 45);
+            this.btn_circle.TabIndex = 2;
+            this.btn_circle.Text = "Circle";
+            this.btn_circle.UseVisualStyleBackColor = true;
             // 
             // holderRight
             // 
@@ -92,39 +159,42 @@
             this.holderRight.Size = new System.Drawing.Size(40, 410);
             this.holderRight.TabIndex = 2;
             // 
-            // btn_new
+            // btn_pen
             // 
-            this.btn_new.Location = new System.Drawing.Point(4, 3);
-            this.btn_new.Name = "btn_new";
-            this.btn_new.Size = new System.Drawing.Size(55, 28);
-            this.btn_new.TabIndex = 2;
-            this.btn_new.Text = "New";
-            this.btn_new.UseVisualStyleBackColor = true;
+            this.btn_pen.Location = new System.Drawing.Point(6, 167);
+            this.btn_pen.Name = "btn_pen";
+            this.btn_pen.Size = new System.Drawing.Size(52, 42);
+            this.btn_pen.TabIndex = 3;
+            this.btn_pen.Text = "Pen";
+            this.btn_pen.UseVisualStyleBackColor = true;
             // 
-            // btn_open
+            // btn_square
             // 
-            this.btn_open.Location = new System.Drawing.Point(65, 3);
-            this.btn_open.Name = "btn_open";
-            this.btn_open.Size = new System.Drawing.Size(55, 27);
-            this.btn_open.TabIndex = 3;
-            this.btn_open.Text = "Open";
-            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_square.Location = new System.Drawing.Point(6, 215);
+            this.btn_square.Name = "btn_square";
+            this.btn_square.Size = new System.Drawing.Size(52, 45);
+            this.btn_square.TabIndex = 1;
+            this.btn_square.Text = "Square";
+            this.btn_square.UseVisualStyleBackColor = true;
             // 
-            // btn_save
+            // drawarenapanel
             // 
-            this.btn_save.Location = new System.Drawing.Point(126, 3);
-            this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(55, 27);
-            this.btn_save.TabIndex = 0;
-            this.btn_save.Text = "Save";
-            this.btn_save.UseVisualStyleBackColor = true;
+            this.drawarenapanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.drawarenapanel.ForeColor = System.Drawing.SystemColors.Control;
+            this.drawarenapanel.Location = new System.Drawing.Point(58, 43);
+            this.drawarenapanel.Name = "drawarenapanel";
+            this.drawarenapanel.Size = new System.Drawing.Size(702, 406);
+            this.drawarenapanel.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawarenapanel);
+            this.Controls.Add(this.btn_pen);
             this.Controls.Add(this.holderRight);
+            this.Controls.Add(this.btn_square);
             this.Controls.Add(this.holderLeft);
             this.Controls.Add(this.holderTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -132,6 +202,7 @@
             this.Text = "Paint";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.holderTop.ResumeLayout(false);
+            this.holderLeft.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -146,6 +217,12 @@
         private System.Windows.Forms.Button btn_open;
         private System.Windows.Forms.Button btn_new;
         private System.Windows.Forms.Panel holderRight;
+        private System.Windows.Forms.Button btn_brush;
+        private System.Windows.Forms.Button btn_pen;
+        private System.Windows.Forms.Button btn_rubber;
+        private System.Windows.Forms.Button btn_circle;
+        private System.Windows.Forms.Button btn_square;
+        private System.Windows.Forms.Panel drawarenapanel;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,12 +20,34 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btn_close_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btn_minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_new_Click(object sender, EventArgs e)
+        {
+            drawarenapanel.Refresh();
+            this.drawarenapanel.BackgroundImage = null;
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "PNG Files|*.png|JPEG Files|*.jpg";
+            if(o.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                drawarenapanel.BackgroundImage = (Image)Image.FromFile(o.FileName).Clone();
+                drawarenapanel.BackgroundImageLayout = ImageLayout.Zoom;
+            }
         }
     }
 }
